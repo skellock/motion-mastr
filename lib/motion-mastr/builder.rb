f2c75a94308df49f99ea2b2b8f0f0c1e6ada201d
Steve Kellock <steve@kellock.ca>
2015-05-02 01:56:33 -0400

    It builds unstyled strings or else it gets the hose.

--- lib/motion-mastr/builder.rb
+++ lib/motion-mastr/builder.rb
@@ -2,18 +2,22 @@ module MotionMastr
 
   class Builder
 
+    attr_reader :default_styles, :pieces
+
     def initialize
+      @default_styles = {}
       @pieces = []
     end
 
     # add a piece of text and optional styles
     def add(text, styles={})
-      @pieces << [text, styles]
+      @pieces << {text: text, styles: styles} unless text.nil?
       self
     end
 
     def build
-      out = NSMutableAttributedString.alloc.init
+      text = @pieces.map { |p| p[:text] }.join
+      out = NSMutableAttributedString.alloc.initWithString(text)
       out
     end
 
--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -1,19 +1,39 @@
 describe "Builder" do
 
-  describe "Fresh Instance" do
+  before do
+    @builder = MotionMastr::Builder.new
+  end
 
-    before do
-      @builder = MotionMastr::Builder.new
-    end
+  it "should build an NSMutableAttributedString" do
+    @builder.build.is_a?(NSMutableAttributedString).should == true
+  end
 
-    it "should build an NSMutableAttributedString" do
-      @builder.build.is_a?(NSMutableAttributedString).should == true
-    end
+  it "should build a 0 length NSMutableAttributedString when empty" do
+    @builder.build.length.should == 0
+  end
 
-    it "should be empty" do
-      @builder.build.length.should == 0
-    end
+  it "should start with no default styles" do
+    @builder.default_styles.keys.length.should == 0
+  end
 
+  it "should chain itself when adding" do
+    @builder.add("hi").should == @builder
+  end
+
+  it "should not add nil text" do
+    @builder.add(nil).pieces.count.should == 0
+  end
+
+  it "should add valid text to the pieces" do
+    @builder.add("hi").pieces.count.should == 1
+  end
+
+  it "should build unstyled text" do
+    @builder.add("hey").build.string.should == "hey"
+  end
+
+  it "should join unstyled pieces together" do
+    @builder.add("a").add("b").add("c").build.string.should == "abc"
   end
 
 end
